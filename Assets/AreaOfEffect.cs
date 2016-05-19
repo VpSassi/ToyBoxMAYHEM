d1fa93ec6d7596126c7b7910354e5d2b585b7d4d
unknown <karin.aimonen@peli31-pc.opisto.hkro>
2016-05-19 13:57:52 +0300

    KA update

---
+++ Assets/AreaOfEffect.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AreaOfEffect : MonoBehaviour
+{
+    List<GameObject> EnemiesInCollider;
+    public float distance = 100;
+    public int gunDamage = 2;
+    bool facingRight;
+    PlayerMovement playerMov;
+    public float CDT;
+    float timeSinceLastCDT;
+    //CDT = CoolDownTime
+    Rigidbody2D rb;
+    public LayerMask mask;
+
+    // public float blastRadius = 50;
+
+
+    void OnCollisionEnter2D(Collision2D c)
+    {
+
+        if (c.gameObject.tag == "Enemy")
+        {
+            EnemiesInCollider.Add(c.gameObject);
+        }
+        facingRight = playerMov.facingRight;
+
+        if (Input.GetKey(KeyCode.X))
+        {
+
+            rb.constraints = RigidbodyConstraints2D.FreezePositionX;
+            rb.freezeRotation = true;
+            playerMov.jumpingPermission = false;
+
+            timeSinceLastCDT += Time.deltaTime;
+            if (timeSinceLastCDT > CDT)
+            {
+                timeSinceLastCDT -= CDT;
+
+                if (facingRight == true && playerMov.onRope == false)
+                {
+                    for (int i = 0; i < EnemiesInCollider.Count; i++)
+                    {
+                        EnemiesInCollider[i].GetComponent<Zombear>().HP -= gunDamage;
+                    }
+
+
+                }
+                else if (facingRight == false && playerMov.onRope == false)
+                {
+
+                }
+            }
+            else {
+                rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+                playerMov.jumpingPermission = true;
+            }
+        }
+    }
+}
