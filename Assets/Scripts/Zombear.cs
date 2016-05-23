4af9ed33c788087142bd64d8c97e7fba869cd078
unknown <ville-petteri.sassi@peli28-pc.opisto.hkro>
2016-05-23 09:22:17 +0300

    VP Update 4

--- Assets/Scripts/Zombear.cs
+++ Assets/Scripts/Zombear.cs
@@ -6,17 +6,45 @@ public class Zombear : MonoBehaviour {
     public int HP = 3;
     PewPew pewPew;
 
+    public bool isHit;
+    public float recovery;
+    public float recoveryCDT;
+    public float hitForce;
+
+    public Sprite enemyHit;
+    Sprite enemyNormal;
+    SpriteRenderer sr;
+
+    Rigidbody2D rbEnemy;
+
 	void Awake () {
         pewPew = GetComponent<PewPew>();
+        sr = GetComponent<SpriteRenderer>();
+        enemyNormal = sr.sprite;
+
 	
 	}
 
 	void Update () {
 
+        if (isHit == true)
+        {
+            sr.sprite = enemyHit;
+            //rbEnemy.MovePosition(transform.position + Vector3.up * hitForce * Time.deltaTime);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(
+            GetComponent<Rigidbody2D>().velocity.x, hitForce);
 
+            recovery += Time.deltaTime;
+            if (recovery > recoveryCDT) {
+                recovery -= recoveryCDT;
+                isHit = false;
+                sr.sprite = enemyNormal;
+            }
+
+        }
 
         if (HP <= 0) {
-            print("Enemy Dead");
+            Destroy(gameObject);
         }
 	
 	}
